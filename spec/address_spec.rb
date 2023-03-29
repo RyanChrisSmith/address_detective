6e5ac3db0d39a251fe543f557056bd6b5ea1f1cb
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-29 11:51:25 -0600

    error handling added for missing attributes when creating Address objects

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -2,19 +2,46 @@ require 'spec_helper'
 require './lib/address'
 
 RSpec.describe Address do
-  before :each do
-    @one_address = Address.new('143 e Maine Street', 'Columbus', '43215')
+  describe '#initialize' do
+    before :each do
+      @one_address = Address.new('143 e Maine Street', 'Columbus', '43215')
+    end
+
+    it 'will make an Address object for each address from the CSV' do
+      expect(@one_address).to be_a Address
+    end
+
+    it 'has attributes' do
+      expect(@one_address.instance_variables).to eq(%i(@street @city @zip_code))
+    end
+
+
+    it 'will make an Address object with incomplete info' do
+      incomplete_1 = Address.new('143 e Maine Street', 'Columbus', '43')
+      incomplete_2 = Address.new('Street', 'Columbus', '43215')
+      incomplete_3 = Address.new('143 e Maine Street', 'C', '43215')
+
+      expect(incomplete_1).to be_a Address
+      expect(incomplete_2).to be_a Address
+      expect(incomplete_3).to be_a Address
+    end
   end
 
-  it 'will make an Address object for each address from the CSV' do
-    expect(@one_address).to be_a Address
+  describe '#to_s' do
+    it 'will return the address as one string' do
+      one_address = Address.new('143 e Maine Street', 'Columbus', '43215')
+      expect(one_address.to_s).to eq("143 e Maine Street, Columbus, 43215")
+    end
   end
 
-  it 'has attributes' do
-    expect(@one_address.instance_variables).to eq(%i(@street @city @zip_code))
+  describe 'error handling' do
+    describe 'empty attributes' do
+      it 'will return an argument error if any one of the attributes are empty' do
+        expect { Address.new('', 'Columbus', '43215') }.to raise_error(ArgumentError, "street can't be blank")
+        expect { Address.new('143 e Maine Street', '', '43215') }.to raise_error(ArgumentError, "city can't be blank")
+        expect { Address.new('143 e Maine Street', 'Columbus', '') }.to raise_error(ArgumentError, "zip code can't be blank")
+      end
+    end
   end
 
-  it 'will return the address as one string' do
-    expect(@one_address.to_s).to eq("143 e Maine Street, Columbus, 43215")
-  end
 end
