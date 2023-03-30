5f27df3051638b797b7cb211012c53bfd61def2d
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-30 10:53:17 -0600

    refactor response_address, separated out attribute assigning that was originally in the initialize to separate methods allowing for any future changes to be easier to adjust

--- lib/response_address.rb
+++ lib/response_address.rb
@@ -1,24 +1,34 @@
+# ResponseAddress represents an address returned from a third-party API.
 class ResponseAddress
-  # Create a reader method for the full_response attribute.
-  attr_reader :full_response
-
-  # Initialize a ResponseAddress instance with address data returned from the SmartyStreets API.
+  # Initialize with all current and potential future attributes by using a method for addition of new ones.
   def initialize(address_data)
-    # If the address data is empty, set the full_response attribute to 'Invalid Address'.
-    if !address_data.empty?
-      # Extract the street, city, zip code, and plus4 code from the address data and set instance variables.
-      @street = address_data[0][:delivery_line_1]
-      @city = address_data[0][:components][:city_name]
-      @zip = address_data[0][:components][:zipcode]
-      @plus4_code = address_data[0][:components][:plus4_code]
-      # Set the full_response attribute to the complete address in the format "street, city, zip-plus4".
-      @full_response = "#{@street}, #{@city}, #{@zip}-#{@plus4_code}"
-    else
-      @full_response = 'Invalid Address'
-    end
+    @address_data = address_data
+  end
+
+  # Return the street address.
+  def street
+    @address_data[0][:delivery_line_1]
+  end
+
+  # Return the city.
+  def city
+    @address_data[0][:components][:city_name]
+  end
+
+  # Return the zip code.
+  def zip
+    @address_data[0][:components][:zipcode]
+  end
+
+  # Return the plus4 code (if any).
+  def plus4_code
+    @address_data[0][:components][:plus4_code]
+  end
+
+  # Return the full response, in the format "street, city, zip-code plus4-code".
+  def full_response
+    return 'Invalid Address' if @address_data.empty?
+
+    "#{street}, #{city}, #{zip}-#{plus4_code}"
   end
 end
-
-#make full_response a method
-#address_data as only attribute in initialize
-#separate methods for each within address_data
