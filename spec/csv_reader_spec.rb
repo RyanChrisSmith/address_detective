cd0ec387a6354253be7fbe4113afb63f9bcb457d
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-30 09:55:43 -0600

    added testing to make sure attributes were assigned correctly, also made testing more dynamic and removed hard coding of retrieving all data from csv file

--- spec/csv_reader_spec.rb
+++ spec/csv_reader_spec.rb
@@ -4,11 +4,12 @@ require './lib/csv_address'
 
 RSpec.describe CsvReader do
   describe 'CSV reader pulling info from the CSV file' do
-    it 'will retrieve all 6 addresses from the test CSV file' do
+    it 'will retrieve all addresses from the test CSV file' do
       reader = CsvReader.new
       addresses = reader.read("./data/test_addresses.csv")
+      expected_count = CSV.read("./data/test_addresses.csv").count - 1
 
-      expect(addresses.count).to eq 6
+      expect(addresses.count).to eq(expected_count)
     end
 
     it 'will return the addresses as Address objects' do
@@ -17,6 +18,15 @@ RSpec.describe CsvReader do
 
       expect(csv_addresses.first).to be_a CsvAddress
       expect(csv_addresses.first.complete).to eq("143 e Maine Street, Columbus, 43215")
+    end
+
+    it 'will set the CsvAddress attributes to the correct values' do
+      reader = CsvReader.new
+      csv_addresses = reader.read("./data/test_addresses.csv")
+
+      expect(csv_addresses.first.street).to eq "143 e Maine Street"
+      expect(csv_addresses.first.city).to eq "Columbus"
+      expect(csv_addresses.first.zip_code).to eq "43215"
     end
   end
 end
