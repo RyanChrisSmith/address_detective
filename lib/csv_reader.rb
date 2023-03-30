31a49307e235887d95a17d78a43938e82e1a1e81
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-03-30 09:56:45 -0600

    refactored csv_reader class to eliminate case and whitespace sensitivity for headers

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -4,13 +4,11 @@ class CsvReader
   # Read the addresses from a CSV file and return an array of CsvAddress objects.
   def read(file_path)
     addresses = []
-    CSV.foreach(file_path, headers: true) do |row|
+    # Read in the CSV file, downcase and strip the headers, and iterate over each row. - Allows for case and whitespace insensitivity
+    CSV.foreach(file_path, headers: true, header_converters: ->(header) { header.downcase.strip }) do |row|
       # Create a new CsvAddress object from the data in each row and add it to the addresses array.
-      addresses << CsvAddress.new(row['Street'], row[' City'], row[' Zip Code'])
+      addresses << CsvAddress.new(row['street'], row['city'], row['zip code'])
     end
     addresses
   end
 end
-
-
-#fix row reading as it comes in, eliminate spaces and capitalization concerns - no parsing errors
