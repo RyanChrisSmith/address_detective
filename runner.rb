e4604c3c25ddbe67f781b891f210e4817e806103
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-04-03 15:10:10 -0600

    added error handling for runner class regarding wrong input in CLI

--- runner.rb
+++ runner.rb
@@ -6,17 +6,27 @@ require './lib/response_address'
 require './lib/smarty_streets_api'
 
 class Runner
-  # Get the CSV file path from the command line arguments
-  csv_file_path = ARGV[0]
+  begin
+    # Get the CSV file path from the command line arguments
+    csv_file_path = ARGV[0]
 
-  # Read in the CSV file
-  csv_reader = CsvReader.new
-  addresses = csv_reader.read(csv_file_path)
+    # Validate CSV file path
+    raise ArgumentError, "CSV file path is required" if csv_file_path.nil? || csv_file_path.strip.empty?
 
-  # Validate each address using the SmartyStreets API
-  addresses.each do |address|
-    response = SmartyStreetsApi.confirm_address(address.street, address.city, address.zip_code)
-    corrected_address = ResponseAddress.new(response)
-    puts "#{address.complete} -> #{corrected_address.full_response}"
+    # Read in the CSV file
+    csv_reader = CsvReader.new
+    addresses = csv_reader.read(csv_file_path)
+
+    # Validate each address using the SmartyStreets API
+    addresses.each do |address|
+      response = SmartyStreetsApi.confirm_address(address.street, address.city, address.zip_code)
+      corrected_address = ResponseAddress.new(response)
+      puts "#{address.complete} -> #{corrected_address.full_response}"
+    end
+  rescue ArgumentError => e
+    puts "Error: #{e.message}"
+  rescue StandardError => e
+    puts "Unexpected error: #{e.message}"
   end
 end
+
