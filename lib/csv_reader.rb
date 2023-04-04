ec8b19a45b32bb269d37e0e5412de2f8b39c6c04
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-04-04 09:37:50 -0600

    refactor and re-comment CsvReader class to eliminate a need for an empty array collector saving memory use

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -5,12 +5,15 @@ require 'csv'
 class CsvReader
   # Read the addresses from a CSV file and return an array of CsvAddress objects.
   def read(file_path)
-    addresses = []
-    # Read in the CSV file, downcase and strip the headers, and iterate over each row. - Allows for case and whitespace insensitivity
-    CSV.foreach(file_path, headers: true, header_converters: ->(header) { header.downcase.strip }) do |row|
-      # Create a new CsvAddress object from the data in each row and add it to the addresses array.
-      addresses << CsvAddress.new(row['street'], row['city'], row['zip code'])
-    end
-    addresses
+    # Use the CSV library to read in the CSV file.
+    # The headers option tells CSV to treat the first row as column headers.
+    # The header_converters option uses a lambda to downcase and strip each header allowing for case and whitespace insensitivity.
+    # The resulting CSV::Table object is returned by CSV.foreach method.
+    # We call the map method on this table object to transform each row into a CsvAddress object
+    # The resulting array of CsvAddress objects is returned by the map method.
+    CSV.foreach(file_path, headers: true, header_converters: ->(header) { header.downcase.strip })
+       .map { |row| CsvAddress.new(row['street'], row['city'], row['zip code']) }
   end
 end
+
+
